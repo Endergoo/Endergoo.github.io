18aa59c873eac9a3fe4fa07ad13a887591c59bdc
Endergoo <hmboone@email.sc.edu>
2025-09-11 12:36:13 -0400

    Example

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -7,7 +7,32 @@ const sayHello = () =>
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () =>
+document.getElementById("btn-click-me").onclick = (event) =>
 {
-    console.log("Hello World");
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked");
 };
+
+document.getElementById("happy-button").onclick = () =>
+    {
+        const pFeel = document.getElementById("p-feeling");
+        pFeel.innerHTML = "This is HAPPY";
+        pFeel.classList.add("hclicked");
+        pFeel.classList.remove("sclicked");
+    };
+
+document.getElementById("sad-button").onclick = () =>
+    {
+        const pFeel = document.getElementById("p-feeling");
+        pFeel.innerHTML = "This is SAD";
+        pFeel.classList.add("sclicked");
+        pFeel.classList.remove("hclicked");
+    };
+
+document.getElementById("clear-button").onclick = () =>
+    {
+        const pFeel = document.getElementById("p-feeling");
+        pFeel.innerHTML="";
+        pFeel.classList.remvove("sclicked");
+        pFeel.classList.remove("hclicked");
+    };
