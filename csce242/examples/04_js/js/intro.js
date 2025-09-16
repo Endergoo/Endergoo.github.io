764c161e6094f8a50c22008803450c3b4526cb7b
Endergoo <hmboone@email.sc.edu>
2025-09-16 12:51:57 -0400

    jnehnce

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -42,4 +42,37 @@ document.getElementById("btn-click-me").onclick = (event) =>
         const userInput = event.currentTarget.value;
         document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
         document.getElementById("img-emotion").classList.remove("hidden");
+    };
+
+    document.getElementById("btn-mood").onclick = () =>
+    {
+        const color = document.getElementById("txt-color").value.trim().toLowerCase();
+        const p = document.getElementById("color-result");
+        p.innerHTML = "";
+        const error = document.getElementById("error-color");
+        error.innerHTML = "";
+        let mood = "";
+
+        if(color == "")
+        {
+            error.innerHTML = "* blank";
+            return;
+        }
+
+        if(color == "red")
+        {
+            mood = "angry";
+        }
+        else if(color == "blue")
+        {
+            mood = "sad";
+        }
+
+        if(mood == "")
+        {
+            error.innerHTML = "* Invalid color";
+            return;
+        }
+
+        p.innerHTML= `You choose ${color}, so you are feeling ${mood}`;
     }
