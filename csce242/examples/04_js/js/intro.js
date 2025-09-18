29af7a09685eba9bd18312b412dc61bfbbe5fffb
Endergoo <hmboone@email.sc.edu>
2025-09-18 13:03:18 -0400

    examples

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -76,3 +76,73 @@ document.getElementById("btn-click-me").onclick = (event) =>
 
         p.innerHTML= `You choose ${color}, so you are feeling ${mood}`;
     }
+
+    /* counter */
+    let counter = 0;
+    let counterInterval;
+    const countP = document.getElementById("p-count");
+    const btnStartCount = document.getElementById("btn-count-start");
+    const btnPauseCount = document.getElementById("btn-count-pause");
+    const btnResetCount = document.getElementById("btn-count-reset");
+    btnPauseCount.disabled = true;
+
+    document.getElementById("btn-count-start").onclick = () =>
+    {
+        btnStartCount.disabled = true;
+        btnPauseCount.disabled = false;
+
+        counterInterval = setInterval(()=>
+        {
+            counter++;
+            countP.innerHTML = counter;
+        }, 1000);
+    }
+
+    document.getElementById("btn-count-pause").onclick = () =>
+    {
+        btnStartCount.disabled = false;
+        btnPauseCount.disabled = true;
+        clearInterval(counterInterval);
+    }
+
+    document.getElementById("btn-count-reset").onclick = () =>
+    {
+        clearInterval();
+    }
+
+    /* Donation stuff */
+    const goal = 10000;
+    document.getElementById("goal-span").innerHTML = goal;
+
+    document.getElementById("btn-donation").onclick = () =>
+    {
+        const donation = document.getElementById("txt-donations").value;
+        const errorSpan = document.getElementById("donation-error");
+        const donoMessage = document.getElementById("dono-message");
+        errorSpan.innerHTML = "";
+        donoMessage.innerHTML = "";
+
+        if(isNaN(donation) || donation <= 0)
+        {
+            errorSpan.innerHTML = "* Invalid Amount."
+            return;
+        }
+
+        const donationPercent = donation / goal * 100;
+
+        if(donationPercent >= 100)
+        {
+            donoMessage.innerHTML = "goal reached";
+        }
+        else if(donationPercent >= 50)
+        {
+            donoMessage.innerHTML = "half way there";
+        }
+        else
+        {
+            donoMessage.innerHTML = "get going";
+        }
+
+        document.querySelector(":root").style.setPropery("--donation-percent",donationPercent + "%");
+    }
+    
